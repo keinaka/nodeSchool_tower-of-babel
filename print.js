a246ac0c2f1cb9fe3f351a0de1e80575e4904097
Kei Nakamura <kei.nakamura@nex.nikkei.com>
2016-12-11 23:44:42 +0900

    ex) 4

---
+++ print.js
@@ -0,0 +1,10 @@
+ let arg1 = process.argv[2];
+ let arg2 = process.argv[3];
+
+ import {PI} from "./Math"
+ import {sqrt} from "./Math"
+ import {square} from "./Math"
+
+  console.log(PI);
+  console.log(sqrt(+arg1));
+  console.log(square(+arg2));
