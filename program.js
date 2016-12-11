2f90c02901943ac877bc490d9d7403021fb7a932
Kei Nakamura <kei.nakamura@nex.nikkei.com>
2016-12-11 23:32:59 +0900

    ex) 3

--- program.js
+++ program.js
@@ -1,3 +1,5 @@
+var util = require('util');
+
 class Character {
   constructor(x, y){
     this.x = x;
@@ -15,8 +17,29 @@ class Character {
   }
 }
 
+class Player extends Character{
+  constructor(x,y,name){
+    super(x,y)
+    this.name = name;
+  }
+  move(dx,dy){
+    this.x += dx;
+    this.y += dy;
+  }
+  toString(){
+    return "name: " + this.name + " " + super.toString();
+  }
+}
+
 var x = process.argv[2];
 var y = process.argv[3];
+var name = process.argv[4];
+
 var character = new Character(+x, +y);
 character.damage();
 console.log(character.toString());
+
+var player = new Player(+x,+y,name);
+player.damage();
+player.move(7,8);
+console.log(player.toString());
